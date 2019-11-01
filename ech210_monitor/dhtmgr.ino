6c96b4740471fa15b1b09dc06d29bf8d532f20c0
SBKILA <11612777+SBKila@users.noreply.github.com>
2019-11-01 15:11:24 +0100

    Add files via upload
    Fix issue if DHT is not present or HS

--- ech210_monitor/dhtmgr.ino
+++ ech210_monitor/dhtmgr.ino
@@ -1,41 +1,49 @@
-/*#define DEBUG_DHT*/
+//#define DEBUG_DHT
 #ifdef DEBUG_DHT
- #define DEBUG_DHT_PRINT(x)  Serial.print (x)
- #define DEBUG_DHT_PRINTLN(x)  Serial.println (x)
+#define DEBUG_DHT_PRINT(x)  Serial.print (x)
+#define DEBUG_DHT_PRINTLN(x)  Serial.println (x)
 #else
- #define DEBUG_DHT_PRINT(x)
- #define DEBUG_DHT_PRINTLN(x)
+#define DEBUG_DHT_PRINT(x)
+#define DEBUG_DHT_PRINTLN(x)
 #endif
 
 boolean performDHTAnalyse = false;
-void setupDHT(){
+void setupDHT() {
   // Initialize temperature sensor
   dht.setup(DHT_PIN, DHTesp::DHT22);
   // read dht data each 20 seconde
-  dhtTicker.attach(20, getDHT);
+  dhtTicker.attach(20, onDHTTicker);
 }
-void onDHTTicker(){
+void onDHTTicker() {
   performDHTAnalyse = true;
 }
-void loopDHT(){
-  if(performDHTAnalyse){
+void loopDHT() {
+  if (performDHTAnalyse) {
     getDHT();
     performDHTAnalyse = false;
   }
 }
 
-void getDHT(){
-    DEBUG_DHT_PRINTLN("DHT getting DHT");
-    DEBUG_DHT_PRINTLN("DHT getting Temperature");
-    int temperature = round(dht.getTemperature()*10);
+void getDHT() {
+  DEBUG_DHT_PRINTLN("DHT getting DHT");
+  DEBUG_DHT_PRINTLN("DHT getting Temperature");
+  int temperature = round(dht.getTemperature() * 10);
+  if (dht.getStatus() == DHTesp::ERROR_NONE) {
     DEBUG_DHT_PRINT(temperature);
     DEBUG_DHT_PRINTLN("°");
-    DEBUG_DHT_PRINTLN("DHT getting Humidity");
-    int humidity = round(dht.getHumidity()*10);
+    setTemperature(temperature);
+  }else {
+    DEBUG_DHT_PRINTLN("Fails to get Temperature");
+  }
+
+  DEBUG_DHT_PRINTLN("DHT getting Humidity");
+  int humidity = round(dht.getHumidity() * 10);
+  if (dht.getStatus() == DHTesp::ERROR_NONE) {
     DEBUG_DHT_PRINT(humidity);
     DEBUG_DHT_PRINTLN("%");
-
     setHumidity(humidity);
-    setTemperature(temperature);
-    
+  } else {
+    DEBUG_DHT_PRINTLN("Fails to get Humidity");
+  }
+
 }
